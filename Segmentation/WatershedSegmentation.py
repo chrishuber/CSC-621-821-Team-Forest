41ccc1dded92e4ad2b872d5364b2591ef1c22593
mato-r <72126927+mato-r@users.noreply.github.com>
2022-03-15 20:18:50 -0700

    Segmentation example

---
+++ Segmentation/WatershedSegmentation.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+# Run with:
+# ./WatershedImageFilter.py <InputFileName> <OutputFileName> <Threshold> <Level>
+# e.g.
+# ./WatershedImageFilter.py BrainProtonDensitySlice.png OutBrainWatershed.png 0.005 .5
+# (A rule of thumb is to set the Threshold to be about 1 / 100 of the Level.)
+#
+#  threshold: absolute minimum height value used during processing.
+#        Raising this threshold percentage effectively decreases the number of local minima in the input,
+#        resulting in an initial segmentation with fewer regions.
+#        The assumption is that the shallow regions that thresholding removes are of of less interest.
+#  level: parameter controls the depth of metaphorical flooding of the image.
+#        That is, it sets the maximum saliency value of interest in the result.
+#        Raising and lowering the Level influences the number of segments
+#        in the basic segmentation that are merged to produce the final output.
+#        A level of 1.0 is analogous to flooding the image up to a
+#        depth that is 100 percent of the maximum value in the image.
+#        A level of 0.0 produces the basic segmentation, which will typically be very oversegmented.
+#        Level values of interest are typically low (i.e. less than about 0.40 or 40%),
+#        since higher values quickly start to undersegment the image.
+
+import itk
+import argparse
+
+parser = argparse.ArgumentParser(description="Segment With Watershed Image Filter.")
+parser.add_argument("input_image")
+parser.add_argument("output_image")
+parser.add_argument("threshold", type=float)
+parser.add_argument("level", type=float)
+args = parser.parse_args()
+
+
+Dimension = 2
+
+FloatPixelType = itk.ctype("float")
+FloatImageType = itk.Image[FloatPixelType, Dimension]
+
+reader = itk.ImageFileReader[FloatImageType].New()
+reader.SetFileName(args.input_image)
+
+gradientMagnitude = itk.GradientMagnitudeImageFilter.New(Input=reader.GetOutput())
+
+watershed = itk.WatershedImageFilter.New(Input=gradientMagnitude.GetOutput())
+
+threshold = args.threshold
+level = args.level
+watershed.SetThreshold(threshold)
+watershed.SetLevel(level)
+
+LabeledImageType = type(watershed.GetOutput())
+
+PixelType = itk.ctype("unsigned char")
+RGBPixelType = itk.RGBPixel[PixelType]
+RGBImageType = itk.Image[RGBPixelType, Dimension]
+
+ScalarToRGBColormapFilterType = itk.ScalarToRGBColormapImageFilter[
+    LabeledImageType, RGBImageType
+]
+colormapImageFilter = ScalarToRGBColormapFilterType.New()
+colormapImageFilter.SetColormap(
+    itk.ScalarToRGBColormapImageFilterEnums.RGBColormapFilter_Jet
+)
+colormapImageFilter.SetInput(watershed.GetOutput())
+colormapImageFilter.Update()
+
+WriterType = itk.ImageFileWriter[RGBImageType]
+writer = WriterType.New()
+writer.SetFileName(args.output_image)
+writer.SetInput(colormapImageFilter.GetOutput())
+writer.Update()
---
+++ Segmentation/readme.md
@@ -0,0 +1,8 @@
+HOW TO RUN
+
+1. pip3 install itk
+2. cd into the segmentation folder.
+3. run python3 WatershedSegmentation.py BrainProtonDensitySlice.png OutBrainWatershed.png 0.005 .5
+
+BrainProtonDensitySlice.png is our input to the program. OutBrainWatershed is our the name of the segmented image the program will produce. 0.005 and .5 are paramters passed to the program that dictate the level of segmentation. Review this URL for more information.
+https://itk.org/ITKExamples/src/Segmentation/Watersheds/SegmentWithWatershedImageFilter/Documentation.html
